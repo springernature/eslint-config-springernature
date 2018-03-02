6fc8d8e8b0e02953d036ad3f7784e9ace60f8479
Alex Kilgour <alexjkilgour@gmail.com>
2018-03-02 12:18:24 +0000

    add plugins

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ module.exports = {
         ecmaVersion: 2015
     },
 
+    // Plugins
+    plugins: [
+        'import',
+        'no-use-extend-native',
+        'node',
+        'promise',
+        'unicorn'
+    ],
+
     // The lint rules
     extends: [
-        'eslint:recommended'
+        'eslint:recommended',
+        'plugin:unicorn/recommended'
     ],
     rules: {
         // Custom
@@ -222,6 +232,60 @@ module.exports = {
             'functionPrototypeMethods': true
         }],
         'yield-star-spacing': ['error', 'both'],
-        'yoda': 'error'
+        'yoda': 'error',
+
+        // eslint-plugin-import
+        // https://github.com/benmosher/eslint-plugin-import
+        'import/default': 'error',
+        'import/export': 'error',
+        'import/first': 'error',
+        'import/named': 'error',
+        'import/namespace': ['error', {
+            allowComputed: true
+        }],
+        'import/no-absolute-path': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-amd': 'error',
+        'import/no-duplicates': 'error',
+        'import/no-extraneous-dependencies': 'error',
+        'import/no-mutable-exports': 'error',
+        'import/no-named-as-default-member': 'error',
+        'import/no-named-as-default': 'error',
+        'import/no-unresolved': ['error', {
+            commonjs: true
+        }],
+        'import/order': 'error',
+        'import/prefer-default-export': 'error',
+        'import/no-unassigned-import': ['error', {
+            allow: [
+                'babel-polyfill',
+                '@babel/polyfill',
+                'babel-register',
+                '@babel/register'
+            ]
+        }],
+
+        // eslint-plugin-no-use-extend-native
+        // https://github.com/dustinspecker/eslint-plugin-no-use-extend-native
+        'no-use-extend-native/no-use-extend-native': 'error',
+
+        // eslint-plugin-node
+        // https://github.com/mysticatea/eslint-plugin-node
+        'node/no-unpublished-bin': 'error',
+        'node/process-exit-as-throw': 'error',
+        'node/no-deprecated-api': 'error',
+
+        // eslint-plugin-promise
+        // https://github.com/xjamundx/eslint-plugin-promise
+        'promise/param-names': 'error',
+        'promise/no-return-wrap': ['error', {
+            allowReject: true
+        }],
+        'promise/no-return-in-finally': 'error',
+
+        // eslint-plugin-unicorn
+        // https://github.com/sindresorhus/eslint-plugin-unicorn
+        'unicorn/prefer-spread': 'off',
+        'unicorn/no-new-buffer': 'off'
     }
 };

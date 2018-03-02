6283526ccc7be422ab81b3e18cb3f4ebdc33fa85
Alex Kilgour <alexjkilgour@gmail.com>
2018-03-02 10:32:52 +0000

    add default config options

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 'use strict';
 
 module.exports = {
+    // Environment
+    // https://eslint.org/docs/user-guide/configuring#specifying-environments
+    env: {
+        es6: true,
+        node: true,
+        browser: true
+    },
+
+    // Parser options
+    // https://eslint.org/docs/user-guide/configuring#specifying-parser-options
+    parserOptions: {
+        ecmaVersion: 2015
+    },
+
+    // The lint rules
     extends: [
         'eslint:recommended'
-    ].map(require.resolve),
+    ],
     rules: {}
 };
